72fa66079ae166c6c167908e1593edf429fb3302
dci-student <Terissa>
2023-10-02 13:30:46 +0200

    commiting the new project

---
+++ test/src/PrintSomeText.java
@@ -0,0 +1,9 @@
+public class PrintSomeText {
+
+public static void main(String [] args){
+// Printing some text to the terminal
+/*The code below simple prints out what is written in the quotation marks, regardlass of what is written */
+        System.out.println("This is my first Java Trial code" + "," + "not bad");
+
+    }
+}
