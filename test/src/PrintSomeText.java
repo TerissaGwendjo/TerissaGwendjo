4e9623cd6253f06852ed8ea06f43820a1783f81b
dci-student <Terissa>
2023-10-02 14:35:07 +0200

    changes made

--- test/src/PrintSomeText.java
+++ test/src/PrintSomeText.java
@@ -2,8 +2,23 @@ public class PrintSomeText {
 
 public static void main(String [] args){
 // Printing some text to the terminal
-/*The code below simple prints out what is written in the quotation marks, regardlass of what is written */
-        System.out.println("This is my first Java Trial code" + "," + "not bad");
+/*The code below simple prints out what is written in the quotation marks, regardless of what is written */
+//        System.out.println("This is my first Java Trial code" + "," + "not bad");
+    PrintSomeText obj = new PrintSomeText();
+    obj.add(12, 14);
+    print("My name is Terissa");
+
+    }
+    public void add (int a, int b){
+    int sum = a + b;
+        System.out.println(sum);
+
+    }
+
+
+    private static void print (String txt1){
+
+          System.out.println(txt1);
 
     }
 }
